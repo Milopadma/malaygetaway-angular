3394e99e7ece8d8314cb0b2fe898429a8e678188
AdityaTanaya <tanayaprodotoxs@gmail.com>
2023-11-21 20:13:02 +0800

    chance name

--- src/app/components/button.component.ts
+++ src/app/components/button.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   selector: 'buttonwicon, buttonwicon',
   template: `
     <div
-      class="flex flex-row text-zinc-800 text-2xl font-light leading-7 tracking-tighter border-softblack max-w-full gap-2.5 px-6 py-2 border-2 border-solid max-md:px-5 cursor-pointer"
+      class="flex flex-row text-softblack text-2xl font-light leading-7 tracking-tighter border-softblack max-w-full gap-2.5 px-6 py-2 border-2 border-solid max-md:px-5 cursor-pointer"
     >
       <button type="submit">Continue</button>
       <img
