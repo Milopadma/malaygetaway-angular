216f1ae76ae17be8f680cb05d9888776893c8f13
mylo <milopadma@yahoo.com>
2023-11-27 12:56:41 +0800

    main page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,11 +12,41 @@ import { DialogueBoxModule } from '../../components/dialoguebox.component';
 @Component({
   selector: 'home',
   template: `
-    <div class="flex flex-row h-full justify-center items-center">
-      <div class="flex flex-col">
-        <div class="flex flex-col text-subtitles">Home</div>
-        <div class="flex flex-col text-titles">
-          This Page is Under Construction
+    <div class="flex flex-row h-full justify-center items-center px-48">
+      <div>
+        <div class="flex flex-col">
+          <div class="flex flex-col text-subtitles">Home</div>
+          <div class="flex flex-col text-titles">
+            This Page is a placeholder, for now, it only displays the major UI
+            flows starting points.
+          </div>
+        </div>
+        <div class="h-12"></div>
+        <div class="flex flex-col gap-12">
+          <div>
+            <button
+              class="text-small underline flex flex-row items-center gap-2"
+              (click)="navigateToPage('/merchant/register')"
+            >
+              > Merchant Registration Flow
+            </button>
+          </div>
+          <div>
+            <button
+              class="text-small underline flex flex-row items-center gap-2"
+              (click)="navigateToPage('/officer')"
+            >
+              > Officer Application Review Flow
+            </button>
+          </div>
+          <div>
+            <button
+              class="text-small underline flex flex-row items-center gap-2"
+              (click)="navigateToPage('/login')"
+            >
+              > Merchant Manage Tourism Products Flow
+            </button>
+          </div>
         </div>
       </div>
       <div class="w-12" id="spacer"></div>
@@ -29,8 +59,8 @@ import { DialogueBoxModule } from '../../components/dialoguebox.component';
 })
 export class Home {
   constructor(private router: Router) {}
-  navigateToNextPage() {
-    this.router.navigate(['/merchant/home']);
+  navigateToPage(page: string) {
+    this.router.navigate([page]);
   }
 }
 
