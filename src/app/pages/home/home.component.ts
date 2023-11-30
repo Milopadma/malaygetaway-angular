037443fa446405813ab3836ffdc2d42f7bd9cd07
mylo <milopadma@yahoo.com>
2023-11-30 16:29:26 +0800

    footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -46,7 +46,7 @@ import { ButtonBorderedModule } from '../../components/buttonbordered.component'
       </div>
       <div class="h-64" id="spacer"></div>
       <!-- featured products section -->
-      <div class="flex flex-col items-center">
+      <div class="flex flex-col items-center px-24">
         <div class="h-12" id="spacer"></div>
         <h1 class="text-titles">Featured Products</h1>
         <div class="h-4" id="spacer"></div>
@@ -88,7 +88,7 @@ import { ButtonBorderedModule } from '../../components/buttonbordered.component'
             <h1 class="text-titles">Explore our services</h1>
             <div class="h-4" id="spacer"></div>
             <span class="text-subtitles"
-              >Discover a wide range of services offered <br>by the best local
+              >Discover a wide range of services offered <br />by the best local
               Malaysian merchants.</span
             >
             <div class="h-4" id="spacer"></div>
@@ -150,6 +150,191 @@ import { ButtonBorderedModule } from '../../components/buttonbordered.component'
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+        <div class="h-64" id="spacer"></div>
+      </div>
+      <!-- customer review section -->
+      <div>
+        <div class="flex flex-col items-center">
+          <h1>Customer Reviews</h1>
+          <div class="h-4" id="spacer"></div>
+          <span class="text-subtitles">
+            See what our customers have to say about their experiences
+          </span>
+          <div class="h-4" id="spacer"></div>
+          <div class="flex flex-row">
+            <buttonunbordered label="View All Reviews"></buttonunbordered>
+            <buttonbordered label="Write a Review"></buttonbordered>
+          </div>
+          <div class="h-12" id="spacer"></div>
+          <div class="flex flex-row gap-12">
+            <div class="flex flex-col items-center">
+              <img src="https://via.placeholder.com/200x200" alt="" />
+              <div class="h-4" id="spacer"></div>
+              <span class="text-subtitles">Customer 1</span>
+              <div class="h-4" id="spacer"></div>
+              <span class="text-paragraph">"I love this product"</span>
+            </div>
+            <div class="flex flex-col items-center">
+              <img src="https://via.placeholder.com/200x200" alt="" />
+              <div class="h-4" id="spacer"></div>
+              <span class="text-subtitles">Customer 2</span>
+              <div class="h-4" id="spacer"></div>
+              <span class="text-paragraph">"I love this product"</span>
+            </div>
+            <div class="flex flex-col items-center">
+              <img src="https://via.placeholder.com/200x200" alt="" />
+              <div class="h-4" id="spacer"></div>
+              <span class="text-subtitles">Customer 3</span>
+              <div class="h-4" id="spacer"></div>
+              <span class="text-paragraph">"I love this product"</span>
+            </div>
+          </div>
+          <div class="h-64" id="spacer"></div>
+        </div>
+        <div>
+          <!-- featured Merchants -->
+          <div class="flex flex-row gap-8 justify-center">
+            <div>
+              <h1>Featured Merchants</h1>
+              <div class="h-4" id="spacer"></div>
+              <p>Discover the best local merchants in Malaysia</p>
+              <div class="h-4" id="spacer"></div>
+              <div class="flex flex-row">
+                <buttonunbordered label="View All Merchants"></buttonunbordered>
+                <buttonbordered label="Become a Merchant"></buttonbordered>
+              </div>
+              <div class="h-12" id="spacer"></div>
+              <div>
+                <div class="flex flex-row justify-between gap-4">
+                  <div class="flex flex-row gap-4">
+                    <img
+                      class="object-contain"
+                      src="https://via.placeholder.com/50x50"
+                      alt=""
+                    />
+                    <div class="flex flex-col">
+                      <h2>Merchant 1</h2>
+                      <small>subtitle 1</small>
+                    </div>
+                  </div>
+                  <p>description</p>
+                </div>
+                <div class="flex flex-row justify-between gap-4">
+                  <div class="flex flex-row gap-4">
+                    <img
+                      class="object-contain"
+                      src="https://via.placeholder.com/50x50"
+                      alt=""
+                    />
+                    <div class="flex flex-col">
+                      <h2>Merchant 1</h2>
+                      <small>subtitle 1</small>
+                    </div>
+                  </div>
+                  <p>description</p>
+                </div>
+                <div class="flex flex-row justify-between gap-4">
+                  <div class="flex flex-row gap-4">
+                    <img
+                      class="object-contain"
+                      src="https://via.placeholder.com/50x50"
+                      alt=""
+                    />
+                    <div class="flex flex-col">
+                      <h2>Merchant 1</h2>
+                      <small>subtitle 1</small>
+                    </div>
+                  </div>
+                  <p>description</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <img
+                class="object-contain"
+                src="https://via.placeholder.com/400x800"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="h-64" id="spacer"></div>
+    </div>
+    <!-- get in touch section -->
+    <div class="flex flex-col items-center justify-center">
+      <div>
+        <h1>Get in touch</h1>
+        <div class="h-4" id="spacer"></div>
+        <p>Contact us for any inquiries or feedback</p>
+        <form action="" class="flex flex-col gap-4">
+          <input
+            type="text"
+            class="text-black placeholder:text-fadedgray text-paragraph leading-7 tracking-tighter whitespace-nowrap border-[color:var(--Soft-Black,#2C2C2C)] max-w-full mt-4 px-5 py-2 border-2 border-solid max-md:pl-1"
+            placeholder="name"
+          />
+          <input
+            type="text"
+            class="text-black placeholder:text-fadedgray text-paragraph leading-7 tracking-tighter whitespace-nowrap border-[color:var(--Soft-Black,#2C2C2C)] max-w-full mt-4 px-5 py-2 border-2 border-solid max-md:pl-1"
+            placeholder="email"
+          />
+          <input
+            type="text"
+            class="text-black placeholder:text-fadedgray text-paragraph leading-7 tracking-tighter whitespace-nowrap border-[color:var(--Soft-Black,#2C2C2C)] max-w-full mt-4 px-5 py-2 border-2 border-solid"
+            placeholder="message"
+          />
+          <div class="h-4" id="spacer"></div>
+          <buttonbordered
+            label="Send Message"
+            class="flex grow-0 self-end"
+          ></buttonbordered>
+        </form>
+      </div>
+    </div>
+    <div class="h-64" id="spacer"></div>
+    <!-- footer -->
+    <div class="flex flex-col items-center justify-center">
+      <div>
+        <div class="flex flex-row gap-8">
+          <div>
+            <h1>Discover Malaysia</h1>
+            <div class="h-4" id="spacer"></div>
+            <p>
+              Discover the best local products and services Malaysia has to
+              offer.
+            </p>
+          </div>
+          <div>
+            <h1>Explore</h1>
+            <div class="h-4" id="spacer"></div>
+            <p>Home</p>
+            <p>Benefits</p>
+            <p>About</p>
+          </div>
+          <div>
+            <h1>Legal</h1>
+            <div class="h-4" id="spacer"></div>
+            <p>Terms & Conditions</p>
+            <p>Privacy Policy</p>
+          </div>
+          <div>
+            <h1>Connect</h1>
+            <div class="h-4" id="spacer"></div>
+            <p>Instagram</p>
+            <p>Facebook</p>
+            <p>Twitter</p>
+            <p>LinkedIn</p>
+          </div>
+        </div>
+        <div class="h-4" id="spacer"></div>
+        <div class="flex flex-row justify-between">
+          <div>
+            <p>© 2023 MalayGetaway</p>
+          </div>
+          <div>
+            <p>Powered by Coffee</p>
           </div>
         </div>
       </div>
