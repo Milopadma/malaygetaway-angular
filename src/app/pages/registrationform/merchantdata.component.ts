b4f328973ac4a3cb3038b7cd5ec4c0246f3ac998
mylo <milopadma@yahoo.com>
2023-12-06 12:58:34 +0800

    form validation

--- src/app/pages/registrationform/merchantdata.component.ts
+++ src/app/pages/registrationform/merchantdata.component.ts
@@ -4,7 +4,7 @@ import { Component, ViewChild } from '@angular/core';
 import { buttonwIconModule } from '../../components/button.component';
 import { ProgressBarComponentModule } from '../../components/form/progressbar.component';
 import { Router, RouterOutlet } from '@angular/router';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormsModule, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'merchantdata-form',
@@ -14,46 +14,76 @@ import { FormsModule, NgForm } from '@angular/forms';
         <h1
           class="text-zinc-800 text-subtitles leading-10 tracking-tighter max-w-[313px] mt-20 max-md:mt-10"
         >
-        Lets get to know each other.
+          Lets get to know each other.
         </h1>
         <input
           type="text"
           name="username"
-          ngModel
+          [(ngModel)]="name"
+          #name="ngModel"
           required
           class="text-black placeholder:text-fadedgray text-paragraph leading-7 tracking-tighter whitespace-nowrap border-[color:var(--Soft-Black,#2C2C2C)] w-[412px] max-w-full mt-4 px-5 py-2 border-2 border-solid max-md:pl-1"
           placeholder="username"
         />
+        <!-- field required error -->
+        <div [hidden]="name.valid || name.pristine" class="text-reject">
+          Please enter a valid username.
+        </div>
+
         <input
           type="password"
           name="password"
-          ngModel
+          [(ngModel)]="password"
+          #password="ngModel"
           required
+          pattern="^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$"
           class="text-black placeholder:text-fadedgray text-paragraph leading-7 tracking-tighter whitespace-nowrap border-[color:var(--Soft-Black,#2C2C2C)] w-[412px] max-w-full mt-4 px-5 py-2 border-2 border-solid max-md:pl-1"
           placeholder="password"
         />
+        <!-- password error -->
+        <div [hidden]="password.valid || password.pristine" class="text-reject">
+          Password must be at least 8 characters, contain letters and numbers.
+        </div>
+
         <input
           type="tel"
           name="contactNumber"
-          ngModel
+          [(ngModel)]="contactNumber"
+          #contactNumber="ngModel"
           required
+          pattern="^[0-9]*$"
           class="text-black placeholder:text-fadedgray text-paragraph leading-7 tracking-tighter whitespace-nowrap border-[color:var(--Soft-Black,#2C2C2C)] w-[412px] max-w-full mt-4 px-5 py-2 border-2 border-solid max-md:pl-1"
           placeholder="your contact number"
         />
+        <!-- phone number error -->
+        <div
+          [hidden]="contactNumber.valid || contactNumber.pristine"
+          class="text-reject"
+        >
+          Please enter a valid phone number.
+        </div>
+
         <input
           type="email"
           name="contactEmail"
-          ngModel
+          [(ngModel)]="contactEmail"
+          #contactEmail="ngModel"
           required
+          pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$"
           class="text-black placeholder:text-fadedgray text-paragraph leading-7 tracking-tighter whitespace-nowrap border-[color:var(--Soft-Black,#2C2C2C)] w-[412px] max-w-full mt-4 px-5 py-2 border-2 border-solid max-md:pl-1"
           placeholder="your contact email"
         />
+        <!-- email error -->
+        <div
+          [hidden]="contactEmail.valid || contactEmail.pristine"
+          class="text-reject"
+        >
+          Please enter a valid email address.
+        </div>
+
         <div class="h-32" id="spacer"></div>
         <div class="flex flex-col items-end" type="submit">
-          <buttonwicon
-            type="submit"
-            label="Continue"
-          ></buttonwicon>
+          <buttonwicon type="submit" label="Continue"></buttonwicon>
           <p
             class="text-softgray text-base font-light leading-5 tracking-tighter whitespace-nowrap"
           >
@@ -81,7 +111,7 @@ export class MerchantDataFormComponent {
   }
 
   navigateToNextPage() {
-    this.router.navigate(['/merchant/register/complete']); 
+    this.router.navigate(['/merchant/register/complete']);
   }
 
   @HostListener('document:keydown.enter', ['$event'])
